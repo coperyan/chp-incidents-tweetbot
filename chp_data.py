7c9d249d0196d94b0da7a351658953c189c8c17e
Ryan Cope <rcope@1800radiator.com>
2021-04-21 22:17:28 -0700

    Added more helper functions to work with local save of data

--- chp_data.py
+++ chp_data.py
@@ -2,29 +2,19 @@ import os
 import pandas as pd
 
 chp_centers_dir = 'data/chp_communications_centers.csv'
-
-#Add incident tweet ID???
-col_list = [
-    'chp_center',
-    'incident_number',
-    'incident_type',
-    'incident_location',
-    'incident_location_description',
-    'incident_lat',
-    'incident_lng',
-    'activity_type',
-    'activity_dt',
-    'activity_num',
-    'activity_text'
-]
+incidents_dir = 'data/incident_data.csv'
 
 def get_chp_centers():
     df = pd.read_csv(chp_centers_dir,header=None)
     return df[0].tolist()
 
 def get_incident_df():
-    df = pd.DataFrame(columns=col_list)
+    df = pd.read_csv(incidents_dir)
     return df
+
+def save_incident_df(df):
+    df.to_csv(incidents_dir,index=None)
+    
 
 #Function to receive DF from scrape, save unique incidents??
 
