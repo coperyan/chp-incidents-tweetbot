f9f2c289c171a04416186877ca7b5074acd66581
Ryan Cope <rcope@1800radiator.com>
2021-04-22 20:07:45 -0700

    Changes to diff data structure

--- chp_data.py
+++ chp_data.py
@@ -2,18 +2,26 @@ import os
 import pandas as pd
 
 chp_centers_dir = 'data/chp_communications_centers.csv'
-incidents_dir = 'data/incident_data.csv'
+incident_dir = 'data/incidents.csv'
+incident_activity_dir = 'data/incident_activity.csv'
 
 def get_chp_centers():
     df = pd.read_csv(chp_centers_dir,header=None)
     return df[0].tolist()
 
 def get_incident_df():
-    df = pd.read_csv(incidents_dir)
+    df = pd.read_csv(incident_dir)
     return df
 
 def save_incident_df(df):
-    df.to_csv(incidents_dir,index=None)
+    df.to_csv(incident_dir,index=None)
+
+def get_incident_activity_df():
+    df = pd.read_csv(incident_activity_dir)
+    return df
+
+def save_incident_activity_df(df):
+    df.to_csv(incident_activity_dir,index=None)
 
 #Updates missing incident-level tweet IDs    
 def refresh_incident_tweet_ids():
@@ -27,7 +35,13 @@ def refresh_incident_tweet_ids():
 
     save_incident_df(df)
 
+#Get all incidents that haven't been tweeted
+def get_untweeted_incidents():
+    return None
 
 #Function to create new tweets about incidents
 
 #Function to create new replies providing updates on incidents
+
+incident_df = get_incident_df()
+incident_activity_df = get_incident_activity_df()
