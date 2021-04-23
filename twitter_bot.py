eb120996623edf86a67a4e4f3859ffcfe04e46ee
Ryan Cope <rcope@1800radiator.com>
2021-04-22 20:08:08 -0700

    Adding api creds for twitter work

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,7 +1,22 @@
 import twitter
+import json
 
+def get_creds():
+    with open('creds.json', 'r') as j:
+        contents = json.loads(j.read())
+    return contents
 
-#twitter bot auth using creds in json file
+def get_twitter_api():
+    creds = get_creds()
+    api = twitter.Api(consumer_key = creds['twitterApiKey'],
+                    consumer_secret = creds['twitterApiSecret'],
+                    access_token_key = creds['twitterAccessToken'],
+                    access_token_secret = creds['twitterAccessSecret'])
+    return api
+
+api = get_twitter_api()
+    
+
 
 #function to add replies to existing incident tweet
 
