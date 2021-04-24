c45838aa2b5b3511454c7a4dbd60dedb0dcb3f3d
Ryan Cope <rcope@1800radiator.com>
2021-04-23 22:08:54 -0700

    Fixing formatting issues

--- twitter_bot.py
+++ twitter_bot.py
@@ -22,12 +22,12 @@ def get_incident_format(incident):
     tweet_text = '{}{}\n'.format(tweet_text,incident['incident_type'])
     if len(incident['incident_location']) > 0:
         tweet_text = '{}{}\n'.format(tweet_text,incident['incident_location'])
-    if len(incident['incident_location_description']) > 0:
+    if len(incident['incident_location_description']) > 0 and incident['incident_location_description'].lower() != incident['incident_location'].lower():
         tweet_text = '{}{}\n'.format(tweet_text,incident['incident_location_description'])
     if len(incident['incident_area']) > 0 and incident['incident_area'] != incident['chp_center']:
-        tweet_text = '{}{}, '.format(tweet_text,incident['incident_area'])
-    tweet_text = '{}{} CHP'.format(tweet_text,incident['chp_center'])
-    tweet_text = '{}\n#CHP #CHP{}'.format(tweet_text,incident['chp_center'].replace(' ',''))
+        tweet_text = '{}{}'.format(tweet_text,incident['incident_area'])
+    #tweet_text = '{}{} CHP'.format(tweet_text,incident['chp_center'])
+    tweet_text = '{}\n\n#CHP #CHP{}'.format(tweet_text,incident['chp_center'].replace(' ',''))
     if len(incident['incident_area']) > 0 and incident['incident_area'] != incident['chp_center']:
         tweet_text='{} #{}'.format(tweet_text,incident['incident_area'].replace(' ',''))
     return tweet_text
@@ -46,18 +46,18 @@ def create_tweet(incident):
     tweet_text = get_incident_format(incident)
     api = get_twitter_api()
     status = api.PostUpdate(tweet_text)
-    return status._json['id_str']
+    return int(status._json['id'])
 
 #Function to create tweet with activity dict
 def create_tweet_reply(activity):
     tweet_text = get_activity_format(activity)
-    incident_tweet_id = activity['incident_tweet_id']
+    incident_tweet_id = int(activity['incident_tweet_id'])
     api = get_twitter_api()
     status = api.PostUpdate(tweet_text,in_reply_to_status_id=incident_tweet_id)
-    return status._json['id_str']
+    return int(status._json['id'])
 
 '''
-test_id = '1385776873980923905'
+test_id = '1385810330920439811'
 api = get_twitter_api()
 test_check = api.GetStatus(test_id)
 
